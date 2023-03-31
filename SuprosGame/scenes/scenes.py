ab3298666858e49d19030359166e44f6c30b7444
OGRESKULL <admin@ogreskull.com>
2023-03-30 19:22:02 -0700

    sncnes more scenesnns i'm so done with scense

--- SuprosGame/scenes/scenes.py
+++ SuprosGame/scenes/scenes.py
@@ -12,8 +12,9 @@ class Scene:
     def update(self, dt: float):
         raise NotImplementedError
 
-    def render(self):
-        raise NotImplementedError
+    def render(self, screen=None):
+        if screen is None:
+            screen = pygame.display.get_surface()
 
     def switch_to_scene(self, next_scene: str):
         self.next_scene = next_scene
