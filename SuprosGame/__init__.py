98b9f729b73e9dafb316cce3edc208cf14a6e787
OGRESKULL <admin@ogreskull.com>
2023-03-30 17:35:46 -0700

    fadsf

--- SuprosGame/__init__.py
+++ SuprosGame/__init__.py
@@ -1,32 +1,30 @@
 import pygame
-import sys
-from SuprosGame.scenes import TitleScene, NewGameScene, LoadGameScene, OptionsScene
+from SuprosGame.scenes.title_scene import TitleScene
 
-class Game:
+pygame.init()
+
+class SuprosGame:
     def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((640, 480))
+        self.screen = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
-        self.scenes = {
-            'title': TitleScene(self),
-            'new_game': NewGameScene(self),
-            'load_game': LoadGameScene(self),
-            'options': OptionsScene(self)
-        }
-        self.current_scene = 'title'
-        self.quit = False
-
-    def run(self):
-        while not self.quit:
-            self.clock.tick(60)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.quit = True
-                else:
-                    self.scenes[self.current_scene].handle_event(event)
-            self.scenes[self.current_scene].update()
-            self.scenes[self.current_scene].draw(self.screen)
-            pygame.display.flip()
+        self.current_scene = TitleScene(self)
 
     def change_scene(self, scene_name):
-        self.current_scene = scene_name
+        if scene_name == 'title':
+            self.current_scene = TitleScene(self)
+
+    def run(self):
+        while True:
+            self.clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+
+                self.current_scene.handle_event(event)
+
+            self.current_scene.update()
+            self.current_scene.render(self.screen)
+
+            pygame.display.flip()
--- SuprosGame/scenes/scenes.py
+++ SuprosGame/scenes/scenes.py
@@ -1,4 +1,4 @@
-from .title_scene import TitleScene
+from SuprosGame.scenes.title_scene import TitleScene
 from .new_game_scene import NewGameScene
 from .load_game_scene import LoadGameScene
 from .options_scene import OptionsScene
