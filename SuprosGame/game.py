369c0526256e517b24803079c446051c42f2d0dc
OGRESKULL <admin@ogreskull.com>
2023-03-30 19:18:39 -0700

    sncnes more scenesnns i'm so done with scense

--- SuprosGame/game.py
+++ SuprosGame/game.py
@@ -1,35 +1,48 @@
 import pygame
 from SuprosGame.scenes.title_scene import TitleScene
 from SuprosGame.scenes.menu_scene import MenuScene
-
+from constants import *
+import sys
 
 class Game:
     def __init__(self):
         pygame.init()
-
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("Supros")
-
         self.clock = pygame.time.Clock()
-
         self.scene = TitleScene(self.screen)
-        self.scene.add_scene("menu", MenuScene(self.screen))
+        self.running = True
 
-    def run(self):
-        running = True
-
-        while running:
+    def game_loop(self):
+        while self.running:
             events = pygame.event.get()
-
             for event in events:
                 if event.type == pygame.QUIT:
-                    running = False
+                    self.running = False
 
             self.scene.handle_events(events)
             self.scene.update()
-            self.scene.render(self.screen)
 
+            if self.scene.next_scene == "menu":
+                self.scene = MenuScene(self.screen)
+
+            self.screen.blit(self.scene.background.image, self.scene.background.rect)
+            self.scene.render_ui()
             pygame.display.update()
-            self.clock.tick(60)
+            self.clock.tick(FPS)
 
+    def quit(self):
         pygame.quit()
+        sys.exit()
+
+    def run(self):
+        clock = pygame.time.Clock()
+        while not self.quit:
+            dt = clock.tick(self.fps) / 1000.0
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.quit = True
+            self.scene.handle_events(event)
+            self.scene.update(dt)
+            self.scene.render()
+            pygame.display.update()
--- SuprosGame/scenes/title_scene.py
+++ SuprosGame/scenes/title_scene.py
@@ -5,6 +5,7 @@ from SuprosGame.ui import Label
 
 class TitleScene(Scene):
     def __init__(self, screen):
+        self.ui_elements = []
         super().__init__(screen)
         self.background = pygame.image.load('..\\SuprosAssets\\images\\backgrounds\\title_screen.png')
         press_start_label = Label(100, 100, "Press any key to start", 24)
@@ -15,7 +16,11 @@ class TitleScene(Scene):
     def handle_events(self, events):
         for event in events:
             if event.type == pygame.KEYDOWN:
-                self.switch_to_scene("menu")
+                self.next_scene = "menu"
+
+
+    def update(self):
+        pass
 
     def add_ui_element(self, element):
         self.ui_elements.append(element)
